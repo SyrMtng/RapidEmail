0e503a37201e9de3c92b33ee9975342cd16659b2
ErickKw <erickkusumaw@gmail.com>
2023-02-01 13:59:38 +0700

    Company and modified contact

--- routes/index.js
+++ routes/index.js
@@ -1,209 +1,212 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var multer = require('multer');
-const moment = require('moment');
+var multer = require("multer");
+const moment = require("moment");
 
 const fileStorage = multer.diskStorage({
-	destination: (req, file, callback) => {
-	  callback(null, '././public/images');
-	},
-	filename: (req, file, callback) => {
-	  callback(null, new Date().getTime() + '-' + file.originalname);
-	}
-  });
+  destination: (req, file, callback) => {
+    callback(null, "././public/images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
 
 const kirim = multer({
-	storage: fileStorage
-})
+  storage: fileStorage,
+});
 /* Get Login */
-router.get('/', function (req,res,next){
-	res.render('login',{title:'Send Email'});
-})
+router.get("/", function (req, res, next) {
+  res.render("login", { title: "Send Email" });
+});
 
 /* GET Halaman Utama */
-router.get('/home', function(req, res, next) {
-	res.render('home', { title: 'Send Email' });
+router.get("/home", function (req, res, next) {
+  res.render("home", { title: "Send Email" });
 });
-router.get('/contact', function(req, res, next) {
-	res.render('contact', { title: 'Send Email' });
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Send Email" });
 });
-router.get('/sendemail', function(req, res, next) {
-	res.render('sendemail', { title: 'Send Email' });
+router.get("/company", function (req, res, next) {
+  res.render("company", { title: "Send Email" });
 });
-router.get('/coba', function(req, res, next) {
-	res.render('sendemail copy', { title: 'Send Email' });
+router.get("/sendemail", function (req, res, next) {
+  res.render("sendemail", { title: "Send Email" });
 });
-
-	router.get('/pengeditfoto', function(req, res, next) {
-		res.render('pengeditfoto', { title: 'Pengedit Foto' });
-	});
-
-router.get('/qq', function(req, res, next) {  
-	Fotos.findAll()
-	  	.then(foto => {
-			res.render('sendemail', { 
-			title: 'Send Email',
-	  		});
-	  	})
-	  	.catch(err => {		
-	  		res.render('sendemail', { 
-			title: 'Send Email',
-			berita: []
-	  		});
-	  	});
-});
-  
-  	router.get('/tambahfoto', function(req, res, next) {
-		res.render('tambahfoto', { title: 'Tambah Foto' });
-	});
-	router.post('/tambahfoto', kirim.array('gambar', 1), function(req, res, next) {
-		// var bilangan = req.body.harga;
-		// var	reverse = bilangan.toString().split('').reverse().join(''),
-		// 	ribuan 	= reverse.match(/\d{1,3}/g);
-		// 	ribuan	= ribuan.join('.').split('').reverse().join('');
-
-		let gambar = req.files[0].filename;
-			let foto = {
-			  judul: req.body.judul,
-			  deskripsi: req.body.deskripsi,
-			  harga: req.body.harga,
-			  gambar: gambar
-		  }
-		  Fotos.create(foto)
-		  .then(data => {
-			  res.redirect('/tokofoto');
-		  })
-		  .catch(err => {
-			  res.render('tambahfoto', { 
-			title: 'Tambah Foto'
-		  	});
-		  });
-	});
-
-router.get('/deletefoto/:id', function(req, res, next) {  
-	var id = parseInt(req.params.id); // /detail/2, /detail/3
-	Fotos.destroy({
-		where: {id: id}
-	})
-	.then(num => {
-		res.redirect('/tokofoto');
-	})
-	.catch(err => {
-		res.json({
-			info: "Error",
-			message: err.message
-		});
-	 });  
+router.get("/coba", function (req, res, next) {
+  res.render("sendemail copy", { title: "Send Email" });
 });
 
-	router.get('/editfoto/:id', function(req, res, next) {  
-		const id = parseInt(req.params.id);
-		Fotos.findByPk(id)
-		.then(foto => {
-			if(foto){
-				res.render('editfoto', { 
-					title: 'Edit Foto',
-					fotos: foto
-			});
-			}else{
-				res.redirect('/tokofoto');
-			}
-			
-		})
-		.catch(err => {
-			res.redirect('/editfoto');
-		});
-	
-	});
-	router.post('/editfoto/:id', kirim.array('gambar', 1), function(req, res, next) {  
-		const id = parseInt(req.params.id);
-		// var bilangan = req.body.harga;
-		// var	reverse = bilangan.toString().split('').reverse().join(''),
-		// 	ribuan 	= reverse.match(/\d{1,3}/g);
-		// 	ribuan	= ribuan.join('.').split('').reverse().join('');
+router.get("/pengeditfoto", function (req, res, next) {
+  res.render("pengeditfoto", { title: "Pengedit Foto" });
+});
 
-		let gambar = req.files[0].filename;
-			let foto = {
-			  judul: req.body.judul,
-			  deskripsi: req.body.deskripsi,
-			  harga: req.body.harga,
-			  gambar: gambar
-		  }
-		Fotos.update(foto, {
-			where: {id: id}
-		})
-		.then(num => {
-			res.redirect('/tokofoto');
-			
-		})
-		.catch(err => {
-			res.json({
-				info: "Error",
-				message: err.message
-			});
-		});
-	
-	});
-	
-router.get('/transaksi', function(req, res, next) {
-	Transaksis.findAll()
-	  	.then(transaksi => {
-			res.render('transaksi', { 
-			title: 'Data Transaksi',
-			transaksis: transaksi
-	  		});
-	  	})
-	  	.catch(err => {		
-	  		res.render('tokofoto', { 
-			title: 'Toko Foto',
-			berita: []
-	  		});
-	  	});
+router.get("/qq", function (req, res, next) {
+  Fotos.findAll()
+    .then((foto) => {
+      res.render("sendemail", {
+        title: "Send Email",
+      });
+    })
+    .catch((err) => {
+      res.render("sendemail", {
+        title: "Send Email",
+        berita: [],
+      });
+    });
 });
-router.post('/transaksi', function(req, res, next) {
-	res.render('sendemail', { title: 'Send Email' });
+
+router.get("/tambahfoto", function (req, res, next) {
+  res.render("tambahfoto", { title: "Tambah Foto" });
 });
-router.get('/deletetransaksi/:id', function(req, res, next) {  
-	var id = parseInt(req.params.id); // /detail/2, /detail/3
-	Transaksis.destroy({
-		where: {id: id}
-	})
-	.then(num => {
-		res.redirect('/transaksi');
-	})
-	.catch(err => {
-		res.json({
-			info: "Error",
-			message: err.message
-		});
-	 });  
+router.post("/tambahfoto", kirim.array("gambar", 1), function (req, res, next) {
+  // var bilangan = req.body.harga;
+  // var	reverse = bilangan.toString().split('').reverse().join(''),
+  // 	ribuan 	= reverse.match(/\d{1,3}/g);
+  // 	ribuan	= ribuan.join('.').split('').reverse().join('');
+
+  let gambar = req.files[0].filename;
+  let foto = {
+    judul: req.body.judul,
+    deskripsi: req.body.deskripsi,
+    harga: req.body.harga,
+    gambar: gambar,
+  };
+  Fotos.create(foto)
+    .then((data) => {
+      res.redirect("/tokofoto");
+    })
+    .catch((err) => {
+      res.render("tambahfoto", {
+        title: "Tambah Foto",
+      });
+    });
+});
+
+router.get("/deletefoto/:id", function (req, res, next) {
+  var id = parseInt(req.params.id); // /detail/2, /detail/3
+  Fotos.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      res.redirect("/tokofoto");
+    })
+    .catch((err) => {
+      res.json({
+        info: "Error",
+        message: err.message,
+      });
+    });
+});
+
+router.get("/editfoto/:id", function (req, res, next) {
+  const id = parseInt(req.params.id);
+  Fotos.findByPk(id)
+    .then((foto) => {
+      if (foto) {
+        res.render("editfoto", {
+          title: "Edit Foto",
+          fotos: foto,
+        });
+      } else {
+        res.redirect("/tokofoto");
+      }
+    })
+    .catch((err) => {
+      res.redirect("/editfoto");
+    });
+});
+router.post(
+  "/editfoto/:id",
+  kirim.array("gambar", 1),
+  function (req, res, next) {
+    const id = parseInt(req.params.id);
+    // var bilangan = req.body.harga;
+    // var	reverse = bilangan.toString().split('').reverse().join(''),
+    // 	ribuan 	= reverse.match(/\d{1,3}/g);
+    // 	ribuan	= ribuan.join('.').split('').reverse().join('');
+
+    let gambar = req.files[0].filename;
+    let foto = {
+      judul: req.body.judul,
+      deskripsi: req.body.deskripsi,
+      harga: req.body.harga,
+      gambar: gambar,
+    };
+    Fotos.update(foto, {
+      where: { id: id },
+    })
+      .then((num) => {
+        res.redirect("/tokofoto");
+      })
+      .catch((err) => {
+        res.json({
+          info: "Error",
+          message: err.message,
+        });
+      });
+  }
+);
+
+router.get("/transaksi", function (req, res, next) {
+  Transaksis.findAll()
+    .then((transaksi) => {
+      res.render("transaksi", {
+        title: "Data Transaksi",
+        transaksis: transaksi,
+      });
+    })
+    .catch((err) => {
+      res.render("tokofoto", {
+        title: "Toko Foto",
+        berita: [],
+      });
+    });
+});
+router.post("/transaksi", function (req, res, next) {
+  res.render("sendemail", { title: "Send Email" });
+});
+router.get("/deletetransaksi/:id", function (req, res, next) {
+  var id = parseInt(req.params.id); // /detail/2, /detail/3
+  Transaksis.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      res.redirect("/transaksi");
+    })
+    .catch((err) => {
+      res.json({
+        info: "Error",
+        message: err.message,
+      });
+    });
 });
 
 router.get("/viewfoto/:id", function (req, res, next) {
-	var id = req.params.id;
-	// var nama = req.params.nama;
-	// const komentarr = await Comment1s.findAll({ where: { idfoto: id } });
-	Fotos.findByPk(id)
-	  .then((foto) => {
-		if (foto) {
-		  res.render("viewfoto", {
-			title: "View Foto",
-			fotos: foto,
-			// comment1s: komentarr,
-		  });
-		} else {
-		  // http 404 not found
-		  res.render("viewfoto", {
-			title: "View Produk",
-			foto: {},
-		  });
-		}
-	  })
-	  .catch((err) => {
-		res.render("viewfoto", {
-		  title: "View Foto",
-		  berita: {},
-		});
-	  });
-  });
+  var id = req.params.id;
+  // var nama = req.params.nama;
+  // const komentarr = await Comment1s.findAll({ where: { idfoto: id } });
+  Fotos.findByPk(id)
+    .then((foto) => {
+      if (foto) {
+        res.render("viewfoto", {
+          title: "View Foto",
+          fotos: foto,
+          // comment1s: komentarr,
+        });
+      } else {
+        // http 404 not found
+        res.render("viewfoto", {
+          title: "View Produk",
+          foto: {},
+        });
+      }
+    })
+    .catch((err) => {
+      res.render("viewfoto", {
+        title: "View Foto",
+        berita: {},
+      });
+    });
+});
 module.exports = router;
